7781e32b39c5768c334b9801833ee6c4774f86e7
Jacob Eldred <jeldred95@gmail.com>
2021-01-06 15:47:01 -0800

    Initial code plan

---
+++ app.js
@@ -0,0 +1,48 @@
+
+
+/*
+
+Construct Tic-Tac-Toe with instances of classes GameBoard and Square, which will handle game state internally.
+
+–––––––––––––––––––––
+Classes
+
+- - - - - - - - -
+Gameboard:
+  Constructor:
+    Populate itself with squares
+    Initiate game
+
+  Methods:
+    (Re)set game
+    Swap active player
+    Check for win (If won, remove all listeners)
+
+  onClick:
+    Check for win
+- - - - - - - - -
+Square:
+  Constructor:
+    Add event listeners on its node
+
+  Methods:
+    Play move (Add symbol to DOM, remove listener)
+
+  onClick:
+    Play move
+- - - - - - - - -
+–––––––––––––––––––––
+
+*/
+
+
+
+
+
+
+
+//Once page loads, create new board with existing tic-tac-table
+
+document.onload = () => {
+
+}
